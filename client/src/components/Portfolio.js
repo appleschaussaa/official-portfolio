b25bc4c660022bcf46b8609bb46599fbd560d7d3
appleschaussaa <robertschauss91@gmail.com>
2023-06-06 18:21:59 -0700

    created server and client folders to add a simple database for user input on the contact page

---
+++ client/src/components/Portfolio.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Box, Container, Typography } from "@mui/material";
+import { Project } from ".";
+
+const Portfolio = () => {
+    return (
+        <>
+            <Container
+                sx={{
+                    border: 1,
+                    // m: 1,
+                    justifyContent: "center",
+                    alignItem: "center",
+                    borderRadius: 3,
+                }}
+            >
+                <Box
+                    sx={{
+                        justifyContent: "center",
+                        alignContent: "center",
+                        marginTop: "40px",
+                    }}
+                >
+                    <Typography
+                        variant="h4"
+                        component="h4"
+                        sx={{
+                            textAlign: "center",
+                            borderRadius: 3,
+                            m: 2,
+                        }}
+                    >
+                        Portfolio page
+                    </Typography>
+                </Box>
+                <Box
+                    sx={{
+                        marginTop: "80px",
+                        // margin: "40px"
+                    }}
+                >
+                    <Project />
+                </Box>
+            </Container>
+        </>
+    );
+};
+
+export default Portfolio;
